24abb64c6298e49fbbe68548edef30fac8293e33
ttgenius <melodyzyz@gmail.com>
2018-08-22 13:23:40 +1200

    added linkdein link

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ window.onload = function() {
     document.getElementById("parent1").onclick = function () {
         location.href = "https://github.com/ttgenius";
     };
-}
+
+    document.getElementById("parent2").onclick = function () {
+        location.href = "https://www.linkedin.com/in/melody-zhu-b9337a141";
+    };
+};
